40e7129d1fdbb0f7eb92afa7f70f5a56c9062122
Shrief Essam <shriefessam1999@gmail.com>
2023-06-10 17:22:01 +0300

    heap class

--- Data Structure/Heap.cpp
+++ Data Structure/Heap.cpp
@@ -1,82 +1,127 @@
 #include<iostream>
 #include <bits/stdc++.h>
 
-
 using namespace std;
-
-#define MAX 1000
 
 class Heap{
     private:
-        int a[MAX];
+        vector<int> vec;
         int size;
+        int cap = 1000;
+        void siftUp(int i);
+        void siftDown(int i);
+        int getParent(int i);
+        int getLeftChild(int i);
+        int getRightChild(int i);
 
     public:
-        Heap(){
-            size=0;
-        }
-
-        int parent(int i){
-            return i%2 == 0 ? (i/2)-1 : i/2;
-        }
-
-        int leftChild(int i){
-            return (2*i)+1;
-        }
-
-        int rightChild(int i){
-            return (2*i)+2;
-        }
-
-        void siftUp(int i){
-            while ( a[i] > a[parent(i)] && i>0) {
-                swap(a[i],a[parent(i)]);
-                i = parent(i);
-            }
-        }
-
-        void siftDown(int i){
-            if(leftChild(i)>size-1 && rightChild(i)>size-1)
-                return;
-
-            int max = leftChild(i);
-            if(rightChild(i)<=size-1){
-                if(a[rightChild(i)]>a[max]){
-                    max = rightChild(i);
-                }
-            } 
-
-            if(a[i]<a[max]){
-                swap(a[i],a[max]);
-                siftDown(max);
-            }else{
-                return;
-            }   
-        }
-
-        void insert(int number){
-            size++;
-            a[size-1]= number;
-            siftUp(size-1);
-        }
-
-
-        int extractMax(){
-            int result = a[0];
-            swap(a[0],a[size-1]);
-            size--;
-            siftDown(0);
-            return result;
-        }
-
-        void remove(int i){
-            a[i] = INT_MAX;
-            siftUp(i);
-            extractMax();
-        }
+        Heap();
+        void buildHeap(vector<int> &vec);
+        int getMax();
+        void insert(int value);
+        int extractMax();
+        void remove(int i);
+        void heapSort(vector<int> &vec);
+        
 };
+
+Heap::Heap(){
+    size=0;
+    vec.resize(cap);
+}
+
+void Heap::buildHeap(vector<int> &vec){
+    this->vec = vec;
+    this->size = vec.size();
+    for(int i=vec.size()/2;i>=0;i--){
+        siftDown(i);
+    }
+}
+
+int Heap::getMax(){
+    return vec[0];
+}
+
+int Heap::getParent(int i){
+    return (i-1)/2;
+}
+
+int Heap::getLeftChild(int i){
+    return (2*i)+1;
+}
+
+int Heap::getRightChild(int i){
+    return (2*i)+2;
+}
+
+void Heap::siftUp(int i){
+    if(i == 0)
+        return;
+        
+    int parent = getParent(i);
+    if(vec[i] > vec[parent]){
+        swap(vec[i],vec[parent]);
+        siftUp(parent);
+    }
+}
+
+void Heap::siftDown(int i){
+    int leftChild = getLeftChild(i);
+    int rightChild = getRightChild(i);
+
+    if(leftChild>size-1 && rightChild>size-1)
+        return;
+    
+    int child = leftChild;
+    if(rightChild<=size-1){
+        child = vec[rightChild] > vec[leftChild] ? rightChild : leftChild ; 
+    } 
+
+    if(vec[child] > vec[i]){
+        swap(vec[child],vec[i]);
+        siftDown(child);
+    }
+}
+
+void Heap::insert(int value){
+    assert(size != cap);
+    vec[size++] = value;
+    siftUp(size-1);
+}
+
+int Heap::extractMax(){
+    assert(size !=0);
+    int max = vec[0];
+    swap(vec[0],vec[size-1]);
+    size--;
+    siftDown(0);
+    return max;
+}
+
+void Heap::remove(int i){
+    vec[i] = INT_MAX;
+    siftUp(i);
+    extractMax();
+}
+
+void Heap::heapSort(vector<int> &vec){
+    buildHeap(vec);
+    for(int i=vec.size()-1;i>=0 ; i--)
+        vec[i] = extractMax();
+}
+
+
 
 
 int main(){
+   vector<int> vec = {3,8,5,10,12,14,1};
+   Heap h;
+
+   h.heapSort(vec);
+
+   for(int i=0;i<vec.size();i++){
+     cout << vec[i] << " ";
+   }   
   
 }
+
